ffae877edfe8eddf345eb96550f935eb95d5d541
arunadv30 <dvaruna97@gmail.com>
2024-08-06 13:25:50 +0530

    api

---
+++ DigitalClock/digital.js
@@ -0,0 +1,7 @@
+
+// document.getElementById("abc").innerHTML=new Date();
+//document.getElementById("abc").innerHTML=new Date().toLocaleTimeString();
+
+setInterval(function(){
+  document.getElementById("abc").innerHTML= new Date().toLocaleTimeString();
+},1000)
---
+++ DigitalClock/functions.js
@@ -0,0 +1,13 @@
+//inbuilt functions
+//setTimeout - executes a provided funtion after certain time
+
+// setTimeout(function(){
+//   console.log("GA")
+// }, 4000)
+
+
+//setInterval- executes a provided function at every interval repeatedly
+
+setInterval(function(){
+  console.log("GA")
+}, 1000)
